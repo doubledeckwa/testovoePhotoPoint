17fddc265d2ec38e490349c39c1559b1616cccb9
sofcknquick <pilotmorozov@gmail.com>
2025-03-24 15:18:30 +0300

    реально последний коммит

--- src/pages/Catalog.tsx
+++ src/pages/Catalog.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import { useState, useEffect } from 'react'
 import { useStore, Product } from '../store/useStore'
 import { MagnifyingGlassIcon, FunnelIcon, ShoppingBagIcon } from '@heroicons/react/24/outline'
 
